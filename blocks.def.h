17c502c081d6d9f5e6c18e0a3c9fcc5a4b1c0509
joeldba <joeldba@github.com>
2021-09-26 15:47:51 -0700

    Changed colors

--- blocks.def.h
+++ blocks.def.h
@@ -2,19 +2,19 @@
 static const Block blocks[] = {
 	/*Icon*/     /*Command*/ 							/*Update Interval*/	/*Update Signal*/
 	/* currently playing song */
-	{"[3]  [8]", "mpc current",								5,			0},		
+	{"[3]  [7]", "mpc current",								5,			0},		
 	/* separator */
 	{"[1]", "printf ''",									-1,			0},		
 	/* network connection status */
-	{"[4]  [8]", "nmcli -t -f name connection show --active",					120,			0},
+	{"[4]  [7]", "nmcli -t -f name connection show --active",					120,			0},
 	/* separator */
 	{"[1]", "printf ''",									-1,			0},		
 	/* volume %  */
-	{"[5]  [8]", "pamixer --get-volume",							5,			0},
+	{"[5]  [7]", "pamixer --get-volume",							5,			0},
 	/* separator */
 	{"[1]", "printf ''",									-1,			0},		
 	/* current time  */
-	{"[6]  [8]", "date '+%I:%M %p'",								60,			0},
+	{"[6]  [7]", "date '+%I:%M %p'",								60,			0},
 	/* "Tux" Linux logo, just for funsies */
 	{"[2]  ", "printf ' '",								-1,			0},
 };
