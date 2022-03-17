d053e64a08c446904745fb038cb03b73b818aea1
joeldba <75224242+joeldba@users.noreply.github.com>
2022-03-17 12:50:40 -0700

    Changed colors

--- blocks.def.h
+++ blocks.def.h
@@ -2,17 +2,23 @@
 static const Block blocks[] = {
 	/*Icon*/     /*Command*/ 							/*Update Interval*/	/*Update Signal*/
 	/* currently playing song */
-	{"  ", "mpc current",								5,			0},		
+	{"[3]  [7]", "mpc current",								5,			0},		
+	/* separator */
+	{"[1]", "printf ''",									-1,			0},
 	/* network connection status */
-	{"  ", "nmcli -t -f name connection show --active",				120,			0},
+	{"[6]  [7]", "nmcli -t -f name connection show --active",				120,			0},
+	/* separator */
+	{"[1]", "printf ''",									-1,			0},
 	/* volume %  */
-	{"  ", "pamixer --get-volume",							5,			0},
+	{"[4]  [7]", "pamixer --get-volume",							5,			0},
+	/* separator */
+	{"[1]", "printf ''",									-1,			0},
 	/* current time  */
-	{"  ", "date '+%I:%M %p '",							60,			0},
+	{"[6]  [7]", "date '+%I:%M %p'",							60,			0},
 	/* "Tux" Linux logo, just for funsies */
-	{"", "printf ' '",								-1,			0},
+	{"[2]  ", "printf ' '",								-1,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
